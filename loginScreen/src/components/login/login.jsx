eba0791e5854e26974b2f5585a7bb12560031d01
WandsonB <wandson.study@gmail.com>
2024-05-03 22:36:32 -0300

    finished component login.jsx

--- loginScreen/src/components/login/login.jsx
+++ loginScreen/src/components/login/login.jsx
@@ -9,11 +9,25 @@ const Login = () => {
             <input type="email" placeholder='E-mail'/>
             <FaUser className="icon"/>
         </div>
+
         <div>
             <input type="password" placeholder='Senha'/>
             <FaLock className="icon"/>
-
         </div>
+
+        <div className="recall-forget">
+            <label>
+                <input type="checkbox" />
+                Lembre de mim
+            </label>
+            <a href="#">Esqueceu a senha?</a>
+        </div>
+
+        <div className="signup-link">
+            <p>Não tem uma conta? <a href="#">Resgistre-se</a></p>
+        </div>
+
+
         <button>Entrar</button>
       </form>
     </div>
